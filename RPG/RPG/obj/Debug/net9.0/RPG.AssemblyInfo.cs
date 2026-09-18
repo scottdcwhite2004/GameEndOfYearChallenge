937ba6eabcd19541aec481bcc331fac544a4e7b9
ScottWhite2004 <scottdcwhite2004@gmail.com>
2026-09-18 01:21:16 +0100

    Ran for the first time

--- RPG/RPG/obj/Debug/net9.0/RPG.AssemblyInfo.cs
+++ RPG/RPG/obj/Debug/net9.0/RPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be3f376d573985856d942cef9020090d253bc00e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bceb1341b9feff64b7fef817513bba47c32cf0b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
